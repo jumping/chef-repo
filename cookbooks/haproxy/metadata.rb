54362d87300fb2c2150a1a7b1b9ed9f16cb1d376
jumping <quzhengping@gmail.com>
2013-01-31 11:55:15 +0800

    deleted unused files

--- cookbooks/haproxy/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "BPO Solutions, LLC."
-maintainer_email "jumping_qu@cn.bposolutions.com"
-license          "Apache 2.0"
-description      "Installs/Configures haproxy"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
